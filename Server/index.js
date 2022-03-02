76f8e5429c202ce0082161ae4fc6020bbfcfa8cc
Mohb1998 <mohebkhaled16@gmail.com>
2022-03-02 14:09:50 +0200

    Port update

--- Server/index.js
+++ Server/index.js
@@ -118,7 +118,7 @@ app.post("/Signin", async(req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT,() => {
     console.log("Server running")
 })
