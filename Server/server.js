926fb54dabf10d547b1c8c3bbc575c052bd35f1a
Mohb1998 <mohebkhaled16@gmail.com>
2022-06-13 21:57:55 +0200

    Preparing for deployment

--- Server/server.js
+++ Server/server.js
@@ -224,6 +224,16 @@ app.post("/Signin", async (req, res) => {
 //   }
 // })
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 /* This is a port that is used to listen to the server. */
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
