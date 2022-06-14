e17d8c0d2ab5abd0a8ad0ce7924c5662f203d395
Mohb1998 <mohebkhaled16@gmail.com>
2022-06-14 09:31:40 +0200

    Preparing for deploy

--- Server/server.js
+++ Server/server.js
@@ -224,15 +224,10 @@ app.post("/Signin", async (req, res) => {
 //   }
 // })
 
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static("../build"))
+}
 
 /* This is a port that is used to listen to the server. */
 const PORT = process.env.PORT || 5000
